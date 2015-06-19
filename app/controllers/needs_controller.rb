d63e911eb0d3e923aa0096699bffd4319d14e860
prathamalag1994 <prathamalag@berkeley.edu>
2015-06-19 12:43:21 -0700

    changed needs

--- app/controllers/needs_controller.rb
+++ app/controllers/needs_controller.rb
@@ -3,5 +3,13 @@ class NeedsController < ApplicationController
   end
  
   def create
+  	@need = Need.new(article_params)
+ 
+  	@need.save
+  	redirect_to '/'
+  end
+  private
+  def article_params
+    params.require(:need).permit(:backend, :mid, :frontend, :ux, :management,:systems, :design, :analytics, :marketing)
   end
 end
